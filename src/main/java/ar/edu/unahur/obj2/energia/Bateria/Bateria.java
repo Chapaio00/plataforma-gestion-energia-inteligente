7f54a1c7973d3273ba29c457e7f48c083f30b981
Chapaio00 <zapallitove00@gmail.com>
2026-07-03 20:00:35 -0300

    bateria con metodos de sistemas perifericos y interfaz sistemainteresado agregada

--- src/main/java/ar/edu/unahur/obj2/energia/Bateria/Bateria.java
+++ src/main/java/ar/edu/unahur/obj2/energia/Bateria/Bateria.java
@@ -1,19 +1,26 @@
 package ar.edu.unahur.obj2.energia.Bateria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ar.edu.unahur.obj2.energia.Exceptions.LimiteDeReservaException;
+import ar.edu.unahur.obj2.energia.SistemasPerifericos.SistemaInteresado;
 
 public class Bateria {
     private String identificador;
     private Integer energia;
+    private List<SistemaInteresado> observadores = new ArrayList<>();
     private static final Integer ReservaMinima = -5000;
-
 
     public Bateria(String identificador, Integer energia) {
         this.identificador = identificador;
         this.energia = energia;
     }
 
-
+    public void agregarObservador(SistemaInteresado sistema){
+        observadores.add(sistema);
+    }
+    
 
     public Integer getEnergia(){
         return this.energia;
---
+++ src/main/java/ar/edu/unahur/obj2/energia/SistemasPerifericos/SistemaInteresado.java
@@ -0,0 +1,8 @@
+package ar.edu.unahur.obj2.energia.SistemasPerifericos;
+
+import ar.edu.unahur.obj2.energia.Bateria.Bateria;
+
+public interface SistemaInteresado {
+
+    void notificar(Bateria bateria, String tipoMovimiento, double cantidad);
+}
