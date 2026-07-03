24c5f1b6a59c21974e076fc402eb700407faa4aa
Chapaio00 <zapallitove00@gmail.com>
2026-07-03 20:30:28 -0300

    agregado AlarmaReservaCritica, inicio de tests

---
+++ src/main/java/ar/edu/unahur/obj2/energia/SistemasPerifericos/AlarmaReservaCritica.java
@@ -0,0 +1,14 @@
+package ar.edu.unahur.obj2.energia.SistemasPerifericos;
+
+import ar.edu.unahur.obj2.energia.Bateria.Bateria;
+
+public class AlarmaReservaCritica implements SistemaInteresado {
+
+    @Override
+    public void notificar(Bateria bateria, String tipoMovimiento, double cantidad) {
+        if (bateria.getEnergia() < 0){
+            System.out.println("Alerta, bateria" + bateria.getIdentificador() + " tiene energia por debajo de su nivel de tolerancia!");
+        }
+    }
+    
+}
